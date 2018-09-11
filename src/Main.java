f1c340c4f05b673e2045a493585eb5686402522f
Sarmad <sarmadsa2@gmail.com>
2018-09-12 00:42:52 +0200

    Implement bogosort

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        Funcs f = new Funcs();
+        //Enter array to be sorted here
+        int[] arr={4,5,6,0,7,8,9,1,2,3};
 
+        BogoSort now=new BogoSort();
+        System.out.print("Unsorted: ");
+        now.display1D(arr);
+
+        now.bogo(arr);
+
+        System.out.print("Sorted: ");
+        now.display1D(arr);
 
     }
 }
